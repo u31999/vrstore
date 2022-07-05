7187731f23bff44a3425c4fe801ed7ddb98ef892
u31999 <u31999@gmail.com>
2022-07-05 11:27:32 -0700

    Redux store setup

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -14,8 +14,9 @@ router.get('/:id', expressAsyncHandler( async (req, res) => {
 
     if(product) {
         res.json(product)
-    }else{
-        res.status(404).json({message: 'Product not found'})
+    } else {
+        res.status(404)
+        throw new Error('Product not Found')
     }
 }))
 
