0699a2b753a5e01f5be238980fb37975d963ddf6
u31999 <u31999@gmail.com>
2022-07-05 08:51:27 -0700

    Set error middleware

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRoutes from './routes/products.js'
+import {notFound, errorHandler} from './middleware/errorMiddleware.js'
 
 dotenv.config()
 
@@ -16,15 +17,14 @@ app.get('/', (req, res) => {
     res.send('Welcome To VR-Store')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
 
+app.use('/api/products', productRoutes)
+
+
+app.use(notFound)
+
+app.use(errorHandler)
 
 app.listen(PORT, console.log(`Server is running in 
 ${process.env.NODE_ENV} on port: ${PORT}`.yellow.bold))
